8c00aa53f0bad84c912202a4248230f15e892497
Kelly Davis <kelly.davis@forestry.io>
2021-11-01 12:51:47 -0400

    trigger

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -13,7 +13,7 @@ const iconSchema: TinaField = {
       options: [
         {
           label: "Kelly",
-          value: "kelly6",
+          value: "kelly7",
         },
         {
           label: "Primary",
